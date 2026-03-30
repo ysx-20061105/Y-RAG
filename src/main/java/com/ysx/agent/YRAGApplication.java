48c0b4875b0fdf23fb28e099708f5872d7c08abc
ysx <3088964573@qq.com>
2026-03-30 10:50:02 +0800

    feat: initial project structure

---
+++ src/main/java/com/ysx/agent/YRAGApplication.java
@@ -0,0 +1,13 @@
+package com.ysx.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YRAGApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YRAGApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ysx/agent/YAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysx.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
